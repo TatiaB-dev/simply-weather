052d410696c29b67986cfaaeeaace6fc4c8110f1
Tatia burdett <tatiaburdett@gmail.com>
2020-12-25 16:17:28 -0600

    add get weather function

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const locationURL = 'https://geo.ipify.org/api/v1';
 const locationKey = 'at_fELojdjZn4q2fyV7OJzuZSk6InEZK';
 
 // Open Weather API url and key
-const weatherURL = 'api.openweathermap.org/data/2.5/weather';
+const weatherURL = 'https://api.openweathermap.org/data/2.5/weather';
 const weatherKey = '3fa398fa8f3f496773abff4d988f09eb';
 
 // Formats the apis query parameters
@@ -14,19 +14,28 @@ function formatQueryParams(params) {
 }
 
 // Gets current location based on IP address
-function getLocation() {
+function getPostalCode() {
     const key = 'apiKey' + '=' + locationKey;
     const url = locationURL + '?' + key;
 
     fetch(url)
         .then(response => response.json())
-        .then(responseJson => console.log(responseJson));
+        .then(responseJson => getWeather(responseJson.location.postalCode));
 }
 
 // Get weather
-function getWeather(location) {
+function getWeather(zipcode) {
+    console.log(zipcode);
 
+    const params = {
+        appid: weatherKey,
+        zip: zipcode
+    }    
+
+    const queryString = formatQueryParams(params);
+    const url = weatherURL + '?' + queryString;
+    console.log(url);
+    fetch(url)
+        .then(response => response.json())
+        .then(responseJson => console.log(responseJson));
 }
-
-
-$(getLocation);
