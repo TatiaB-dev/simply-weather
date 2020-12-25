5e57fa558483933cb0235b7ee578b326c0c2b7af
Tatia burdett <tatiaburdett@gmail.com>
2020-12-25 15:33:15 -0600

    add get location function

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const ipURL = 'https://api.ipify.org?format=json';
+// API URLs and Keys
+const locationURL = 'https://geo.ipify.org/api/v1';
+const locationKey = 'at_fELojdjZn4q2fyV7OJzuZSk6InEZK';
 
-function getIPAddress() {
-    fetch(ipURL) 
+// Formats the apis query parameters
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
+}
+
+// Gets current location based on IP address
+function getLocation() {
+    const key = 'apiKey' + '=' + locationKey;
+    const url = locationURL + '?' + key;
+
+    fetch(url)
         .then(response => response.json())
         .then(responseJson => console.log(responseJson));
 }
+
+
+$(getLocation);
