52eed785c7219a8f3203f7fb097723dee9300d24
Tatia burdett <tatiaburdett@gmail.com>
2020-12-30 13:19:13 -0600

    clean up code

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-'use strict';
+"use strict";
 
 // GLOBAL VARIABLES - API URLs and Keys // 
 
     // IP Geolocation API url and key
-const ipGeoLocateURL = 'https://geo.ipify.org/api/v1';
-const ipGeoLocateKey = 'at_fELojdjZn4q2fyV7OJzuZSk6InEZK';
+const ipGeoLocateURL = "https://geo.ipify.org/api/v1";
+const ipGeoLocateKey = "at_fELojdjZn4q2fyV7OJzuZSk6InEZK";
 
     // Open Weather API url and key
-const currentWeatherURL = 'https://api.openweathermap.org/data/2.5/weather';
-const forecastURL = 'https://api.openweathermap.org/data/2.5/forecast';
-const weatherKey = '3fa398fa8f3f496773abff4d988f09eb';
+const currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather";
+const forecastURL = "https://api.openweathermap.org/data/2.5/forecast";
+const weatherKey = "3fa398fa8f3f496773abff4d988f09eb";
 
 // General Formatting Functions //
 
 function formatQueryParams(params) {
     const queryItems = Object.keys(params)
         .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-    return queryItems.join('&');
+    return queryItems.join("&");
 }
 
 function getWeekday(unixTime) {
     let date = new Date(unixTime * 1000);
     let day = date.getDay();
-    const weekdays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    const weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     return weekdays[day];
 }
 
@@ -32,36 +32,36 @@ function getDate(unixTime) {
     let day = date.getDate();
     let year = date.getFullYear();
     let hour = date.getHours();
-    let minutes = ('0' + date.getMinutes()).slice(-2);
-    let amOrPM = 'am';
+    let minutes = ("0" + date.getMinutes()).slice(-2);
+    let amOrPM = "am";
 
     if (hour >= 12) {
-        amOrPM = 'pm';
+        amOrPM = "pm";
     }
     hour = (hour % 12) || 12;
 
-    let finalTime = hour + ':' + minutes + amOrPM
-    let finalDate = month + '/' + day + '/' + year
-    return finalDate + ' ' + finalTime;
+    let finalTime = hour + ":" + minutes + amOrPM
+    let finalDate = month + "/" + day + "/" + year
+    return finalDate + " " + finalTime;
 }
 
 function getWeatherIcon(icon, conditions) {
-    return `<img src='http://openweathermap.org/img/wn/${icon}.png' alt='${conditions}'>`;
+    return `<img src="https://openweathermap.org/img/wn/${icon}.png" alt="${conditions}">`;
 }
 
 function getCityName(city) {
-    return $('#js-city-header').html(`Hello, ${city}`);
+    return $("#js-city-header").html(`Hello, ${city}`);
 }
 
 function getMiles(meters) {
-    return (meters * 0.000621371192).toFixed(1) + 'mi';
+    return (meters * 0.000621371192).toFixed(1) + "mi";
 }
 
 // Fetch IP Geolocation API Request Functions //
 
 function getPostalCode() {
-    const key = 'apiKey' + '=' + ipGeoLocateKey;
-    const url = ipGeoLocateURL + '?' + key;
+    const key = "apiKey" + "=" + ipGeoLocateKey;
+    const url = ipGeoLocateURL + "?" + key;
 
     fetch(url)
         .then(response => {
@@ -72,7 +72,7 @@ function getPostalCode() {
         })
         .then(responseJson => getWeather(responseJson.location.postalCode))
         .catch(err => {
-            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+            $("#js-error-message").text(`Something went wrong: ${err.message}`);
         })
 }
 
@@ -83,11 +83,11 @@ function getWeather(zipcode) {
     const params = {
         appid: weatherKey,
         zip: zipcode,
-        units: 'imperial'
+        units: "imperial"
     }    
 
     const queryString = formatQueryParams(params);
-    const url = currentWeatherURL + '?' + queryString;
+    const url = currentWeatherURL + "?" + queryString;
     fetch(url)
         .then(response => {
             if (response.ok) {
@@ -97,7 +97,7 @@ function getWeather(zipcode) {
         })
         .then(responseJson => displayCurrentWeather(responseJson))
         .catch(err => {
-            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+            $("#js-error-message").text(`Something went wrong: ${err.message}`);
         })
 }
 
@@ -105,11 +105,11 @@ function getWeatherByCity(city) {
     const params = {
         appid: weatherKey,
         q: city,
-        units: 'imperial'
+        units: "imperial"
     }
 
     const queryString = formatQueryParams(params);
-    const url = currentWeatherURL + '?' + queryString;
+    const url = currentWeatherURL + "?" + queryString;
     fetch(url)
         .then(response => {
             if (response.ok) {
@@ -119,7 +119,7 @@ function getWeatherByCity(city) {
         })
         .then(responseJson => displayCurrentWeather(responseJson))
         .catch(err => {
-            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+            $("#js-error-message").text(`Something went wrong: ${err.message}`);
         })
 }
 
@@ -128,11 +128,11 @@ function getForecast(city) {
     const params = {
         appid: weatherKey,
         q: city,
-        units: 'imperial'
+        units: "imperial"
     }    
 
     const queryString = formatQueryParams(params);
-    const url = forecastURL + '?' + queryString;
+    const url = forecastURL + "?" + queryString;
     fetch(url)
         .then(response => {
             if (response.ok) {
@@ -142,7 +142,7 @@ function getForecast(city) {
         })
         .then(responseJson => displayForecast(responseJson))
         .catch(err => {
-            $('#js-error-message').text(`Something went wrong: ${err.message}`);g
+            $("#js-error-message").text(`Something went wrong: ${err.message}`);g
         })
 }
 
@@ -150,9 +150,9 @@ function getForecast(city) {
 
 function displayForecast(responseJson) {
 
-    $('#js-results').removeClass('results');
-    $('#js-forecast-btn').addClass('hidden');
-    $('#js-current-btn').removeClass('hidden');
+    $("#js-results").removeClass("results");
+    $("#js-forecast-btn").addClass("hidden");
+    $("#js-current-btn").removeClass("hidden");
 
     for (let i = 0; i < responseJson.list.length; i += 8) {
         const date = getDate(responseJson.list[i].dt);
@@ -160,15 +160,15 @@ function displayForecast(responseJson) {
         const icon = responseJson.list[i].weather[0].icon;
         const conditions = responseJson.list[i].weather[0].description;
 
-        $('#js-results').append(`
-            <section class='forecast'>
-                <div class='card'>
+        $("#js-results").append(`
+            <section class="forecast">
+                <div class="card">
                     <h2>${weekday}</h2>
                     <p>${date}</p>
-                    <section class='weather-temp'>
+                    <section class="weather-temp">
                         <div>${getWeatherIcon(icon, conditions)}</div>
-                        <div><h3 class='temp'>${Math.round(responseJson.list[i].main.temp)}&degF</h3></div>
-                        <div><p class='feels-like'>(Feels like ${Math.round(responseJson.list[i].main.feels_like)}&degF)</p></div>
+                        <div><h3 class="temp">${Math.round(responseJson.list[i].main.temp)}&degF</h3></div>
+                        <div><p class="feels-like">(Feels like ${Math.round(responseJson.list[i].main.feels_like)}&degF)</p></div>
                     </section>
                 </div>
             </section>
@@ -178,9 +178,9 @@ function displayForecast(responseJson) {
 
 function displayCurrentWeather(responseJson) {
 
-    $('#js-forecast-btn').removeClass('hidden');
-    $('#js-current-btn').addClass('hidden');
-    $('#js-results').addClass('results');
+    $("#js-forecast-btn").removeClass("hidden");
+    $("#js-current-btn").addClass("hidden");
+    $("#js-results").addClass("results");
 
     const temp = Math.round(responseJson.main.temp);
     const feelsLike = Math.round(responseJson.main.feels_like);
@@ -195,21 +195,21 @@ function displayCurrentWeather(responseJson) {
 
     getCityName(responseJson.name);
 
-    $('#js-results').append(
-        `<div class='card current-weather'>
+    $("#js-results").append(
+        `<div class="card current-weather">
             <h2>The weather right now is... </h2>
             <container>
-                <section class='weather-temp'>
+                <section class="weather-temp">
                     <div>${getWeatherIcon(icon, conditions)}</div>
-                    <div><h3 class='temp'>${temp}&degF</h3></div>
-                    <div><p class='feels-like'>(Feels like ${feelsLike}&degF)</p></div>
+                    <div><h3 class="temp">${temp}&degF</h3></div>
+                    <div><p class="feels-like">(Feels like ${feelsLike}&degF)</p></div>
                 </section>
                 <section>
-                    <ul class='weather-list'>
+                    <ul class="weather-list">
                         <li>Humidity: ${responseJson.main.humidity}%</li>
                         <li>Visibility: ${miles}</li>
                         <li>Sunrise: ${sunrise}</li>
-                        <li class='break'>Sunset: ${sunset}</li>
+                        <li class="break">Sunset: ${sunset}</li>
                         <li>Wind Speed: ${wind}m/h</li>
                     </ul>
                 </section>
@@ -222,11 +222,11 @@ function displayCurrentWeather(responseJson) {
 
 function handleFormSubmit() {
 
-    $('#js-find-btn').on('click', function(event) {
+    $("#js-find-btn").on("click", function(event) {
         event.preventDefault();
-        $('#js-results').empty();
-        $('#js-error-message').empty();
-        const input = $('#js-search-city').val()
+        $("#js-results").empty();
+        $("#js-error-message").empty();
+        const input = $("#js-search-city").val()
 
         if (parseInt(input) == input) { // This tests to see if integer, if so, getWeather(zipcode), else getWeatherByCity
             getWeather(input);
@@ -237,22 +237,22 @@ function handleFormSubmit() {
 }
 
 function handleForecastBtn() {
-    $('#js-forecast-btn').on('click', function(event) {
+    $("#js-forecast-btn").on("click", function(event) {
         event.preventDefault();
-        $('#js-results').empty();
-        const getCity = $('#js-city-header').text();
-        const city = getCity.replace('Hello, ',''); 
+        $("#js-results").empty();
+        const getCity = $("#js-city-header").text();
+        const city = getCity.replace("Hello, ",""); 
 
         getForecast(city);
     })
 }
 
 function handleCurrentWeatherBtn() {
-    $('#js-current-btn').on('click', function(event) {
+    $("#js-current-btn").on("click", function(event) {
         event.preventDefault();
-        $('#js-results').empty();
-        const getCity = $('#js-city-header').text();
-        const city = getCity.replace('Hello, ','');
+        $("#js-results").empty();
+        const getCity = $("#js-city-header").text();
+        const city = getCity.replace("Hello, ","");
 
         getWeatherByCity(city);
     })
